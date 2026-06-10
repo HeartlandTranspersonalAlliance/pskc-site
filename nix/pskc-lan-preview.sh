04067b897efbe748d028b983c1e7dff144e44185
Dale Morgan <mail@dalemorgan.us>
2026-06-10 00:13:04 -0500

    Fix nginx preview daemon detachment

--- nix/pskc-lan-preview.sh
+++ nix/pskc-lan-preview.sh
@@ -86,7 +86,7 @@ fi
 
 runtime_base="${PSKC_RUNTIME_DIR:-${XDG_RUNTIME_DIR:-}}"
 if [[ -z "$runtime_base" || ! -d "$runtime_base" || ! -w "$runtime_base" ]]; then
-  runtime_base="${TMPDIR:-/tmp}"
+  runtime_base="/tmp"
 fi
 runtime_dir="$runtime_base/pskc-site-nginx-$port"
 conf_file="$runtime_dir/nginx.conf"
@@ -176,13 +176,19 @@ prepare_runtime() {
 close_extra_fds() {
   local fd fd_dir fd_path
 
-  fd_dir="/proc/$$/fd"
-  [[ -d "$fd_dir" ]] || return 0
+  if [[ -d "/proc/$$/fd" ]]; then
+    fd_dir="/proc/$$/fd"
+  elif [[ -d "/dev/fd" ]]; then
+    fd_dir="/dev/fd"
+  else
+    return 0
+  fi
 
   for fd_path in "$fd_dir"/*; do
     fd="${fd_path##*/}"
     [[ "$fd" =~ ^[0-9]+$ ]] || continue
     [[ "$fd" -gt 2 ]] || continue
+    [[ "$fd" -ne 255 ]] || continue
     eval "exec ${fd}>&-"
   done
 }
